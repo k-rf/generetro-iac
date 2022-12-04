c00f282e17a856257340f9da8f5762909ef3df29
Kawano Kenichi <kenichi.k.820@gmail.com>
2022-12-04 14:52:08 +0000

    :fire: Remove unnecessary environment variable

--- src/variables/config/env.ts
+++ src/variables/config/env.ts
@@ -6,12 +6,6 @@ export type EnvConfig = {
 
   NOTION_TEMPLATE_DAILY_ID: string;
   NOTION_TEMPLATE_WEEKLY_ID: string;
-
-  NOTION_REMIND_COLUMN: string;
-  NOTION_TAG_COLUMN: string;
-  NOTION_ELEMENT_COLUMN: string;
-  NOTION_TAG_DAILY: string;
-  NOTION_TAG_WEEKLY: string;
 };
 
 const envConfig = new pulumi.Config("env");
@@ -22,10 +16,4 @@ export const ENV_CONFIG: EnvConfig = {
 
   NOTION_TEMPLATE_DAILY_ID: envConfig.require("notion-template-daily-id"),
   NOTION_TEMPLATE_WEEKLY_ID: envConfig.require("notion-template-weekly-id"),
-
-  NOTION_REMIND_COLUMN: envConfig.require("notion-remind-column"),
-  NOTION_TAG_COLUMN: envConfig.require("notion-tag-column"),
-  NOTION_ELEMENT_COLUMN: envConfig.require("notion-element-column"),
-  NOTION_TAG_DAILY: envConfig.require("notion-tag-daily"),
-  NOTION_TAG_WEEKLY: envConfig.require("notion-tag-weekly"),
 };
